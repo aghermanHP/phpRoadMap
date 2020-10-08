715655cd72c13185da9a2c7947da1b656bc6ea28
aghermanHP <adrian.gherman1@gmail.com>
2020-10-08 08:35:51 +0300

    was aded 3 entity and datafixtures

--- phpRoadmapProject/api/src/Entity/Affiliate.php
+++ phpRoadmapProject/api/src/Entity/Affiliate.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
@@ -63,10 +64,145 @@ class Affiliate
      */
     private $categories;
 
+    public function __construct()
+    {
+        $this->categories = new ArrayCollection();
+    }
+
+    /**
+     * @return int
+     */
+    public function getId() : ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl() : ?string
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param string $url
+     *
+     * @return self
+     */
+    public function setUrl(string $url) : self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail() : ?string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     *
+     * @return self
+     */
+    public function setEmail(string $email) : self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getToken() : ?string
+    {
+        return $this->token;
+    }
+
+    /**
+     * @param string|null $token
+     *
+     * @return self
+     */
+    public function setToken(?string $token) : self
+    {
+        $this->token = $token;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isActive() : ?bool
+    {
+        return $this->active;
+    }
+
+    /**
+     * @param bool $active
+     *
+     * @return self
+     */
+    public function setActive(bool $active) : self
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt() : ?\DateTime
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @return Category[]|ArrayCollection
+     */
+    public function getCategories()
+    {
+        return $this->categories;
+    }
+
+    /**
+     * @param Category $category
+     *
+     * @return self
+     */
+    public function addCategory(Category $category) : self
+    {
+        if (!$this->categories->contains($category)) {
+            $this->categories->add($category);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param Category $category
+     *
+     * @return self
+     */
+    public function removeCategory(Category $category) : self
+    {
+        $this->categories->removeElement($category);
+
+        return $this;
+    }
+
     /**
      * @ORM\PrePersist
      */
-    public function prePersist()
+    public function prePersist(): void
     {
         $this->createdAt = new \DateTime();
     }
