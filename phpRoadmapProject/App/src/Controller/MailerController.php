750b4719e9e90075faf03c2f665e99bffa8a1bbc
aghermanHP <adrian.gherman1@gmail.com>
2021-02-10 18:34:32 +0200

    create mail controller

---
+++ phpRoadmapProject/App/src/Controller/MailerController.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportException;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class MailerController extends AbstractController
+{
+    /**
+     * @Route("/email")
+     */
+    public function sendEmail(MailerInterface $mailer)
+    {
+        $email = (new Email())
+            ->from('hello@example.com')
+            ->to('you@example.com')
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+
+
+        try {
+            $mailer->send($email);
+        } catch (TransportExceptionInterface $e) {
+            dd($e);
+        }
+        return new Response(' ', Response::HTTP_OK);
+
+    }
+}
