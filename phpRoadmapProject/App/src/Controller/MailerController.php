a0addebc56dced80ab2dc7cd148e0f1bda535c15
aghermanHP <adrian.gherman1@gmail.com>
2021-02-11 11:01:12 +0200

    correct name of the file

--- phpRoadmapProject/App/src/Controller/MailerController.php
+++ phpRoadmapProject/App/src/Controller/MailerController.php
@@ -3,9 +3,9 @@
 
 namespace App\Controller;
 
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mailer\Exception\TransportException;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
@@ -19,13 +19,14 @@ class MailerController extends AbstractController
      */
     public function sendEmail(MailerInterface $mailer)
     {
-        $email = (new Email())
+        $email = (new TemplatedEmail())
             ->from('hello@example.com')
             ->to('you@example.com')
             ->subject('Time for Symfony Mailer!')
             ->text('Sending emails is fun again!')
             ->attachFromPath(__DIR__."/test.txt", 'Privacy Policy')
-            ->html('<p>See Twig integration for better HTML integration!</p>');
+            ->htmlTemplate("test/testTemplate.html.twig");
+//            ->html('<p>See Twig integration for better HTML integration!</p>');
 
 
 
